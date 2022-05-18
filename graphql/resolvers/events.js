98e6315fd25e4c42870d43e4f527e8ab8f66a051
artemdol25 <artemdol25@mail.ru>
2022-05-19 01:52:03 +0300

    добавление функции авторизации

--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -1,38 +1,42 @@
 const Event = require('../../models/event');
+const User = require('../../models/user');
 
 const { transformEvent } = require('./merge');
 
 module.exports = {
-    events: async () => {
-      try {
-        const events = await Event.find();
-        return events.map(event => {
-          return transformEvent(event);
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events.map(event => {
+        return transformEvent(event);
         });
       } catch (err) {
         throw err;
       }
     },
-    createEvent: async args => {
+    createEvent: async (args, req) => {
+      if (!req.isAuth) {
+        throw new Error('Unauthenticated!');
+      }
       const event = new Event({
         title: args.eventInput.title,
         description: args.eventInput.description,
         price: +args.eventInput.price,
         date: new Date(args.eventInput.date),
-        creator: '62855c679dc5687b5a88e3ee'
+        creator: req.userId
       });
       let createdEvent;
       try {
         const result = await event.save();
         createdEvent = transformEvent(result);
-        const creator = await User.findById('62855c679dc5687b5a88e3ee');
-
+        const creator = await User.findById(req.userId);
+  
         if (!creator) {
-          throw new Error('Пользователь не найден.');
+          throw new Error('User not found.');
         }
         creator.createdEvents.push(event);
         await creator.save();
-
+  
         return createdEvent;
       } catch (err) {
         console.log(err);
