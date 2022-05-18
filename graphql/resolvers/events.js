e08a75232f2c009a8bf48a897301e3eed5df32d8
artemdol25 <artemdol25@mail.ru>
2022-05-19 01:22:01 +0300

    рефакторинг проекта

---
+++ graphql/resolvers/events.js
@@ -0,0 +1,42 @@
+const Event = require('../../models/event');
+
+const { transformEvent } = require('./merge');
+
+module.exports = {
+    events: async () => {
+      try {
+        const events = await Event.find();
+        return events.map(event => {
+          return transformEvent(event);
+        });
+      } catch (err) {
+        throw err;
+      }
+    },
+    createEvent: async args => {
+      const event = new Event({
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: new Date(args.eventInput.date),
+        creator: '62855c679dc5687b5a88e3ee'
+      });
+      let createdEvent;
+      try {
+        const result = await event.save();
+        createdEvent = transformEvent(result);
+        const creator = await User.findById('62855c679dc5687b5a88e3ee');
+
+        if (!creator) {
+          throw new Error('Пользователь не найден.');
+        }
+        creator.createdEvents.push(event);
+        await creator.save();
+
+        return createdEvent;
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    }
+  };
