e1c72c409faa1a4fca1c5672d3c81e6021246509
artemdol25 <artemdol25@mail.ru>
2022-05-19 17:45:21 +0300

    сборка проекта

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const mongoose = require('mongoose');
+const path = require('path');
 
 const graphQlSchema = require('./graphql/schema/index');
 const graphQlResolvers = require('./graphql/resolvers/index');
@@ -10,10 +11,14 @@ const isAuth = require('./middleware/is-auth');
 
 const app = express();
 
+const publicPath = path.join(__dirname, './frontend', 'build');
+
 const port = process.env.PORT || 8000;
 const mongodbUri = process.env.MONGODB_URI || 'mongodb+srv://artemdol:4sAZ1emHfSsUHkr0@cluster0.l6zpr.mongodb.net/events-react-dev?retryWrites=true&w=majority';
 
 app.use(bodyParser.json());
+
+app.use(express.static(publicPath));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -26,6 +31,10 @@ app.use((req, res, next) => {
 });
 
 app.use(isAuth);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+ });
 
 app.use(
     '/graphql', 
